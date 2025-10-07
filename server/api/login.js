95a2a089c9060bfa10b4c7a5e5b3c95c581b2342
RolandMario <rolandmario2@gmail.com>
2025-10-08 00:24:56 +0100

    adjust cors preflight

--- server/api/login.js
+++ server/api/login.js
@@ -13,6 +13,12 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
