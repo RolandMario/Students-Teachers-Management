fb3521be010ad43bf1eb94252fdb5d2a3aa45d89
RolandMario <rolandmario2@gmail.com>
2025-10-09 23:51:11 +0100

    update

--- server/api/getStudentsByClass.js
+++ server/api/getStudentsByClass.js
@@ -19,7 +19,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  const {currentClass} = req.query.currentClass
+  const {currentClass} = req.query
   if (req.method !== 'GET') return res.status(405).end();
 
   try {
