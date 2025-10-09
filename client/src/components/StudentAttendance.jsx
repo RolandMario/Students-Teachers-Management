e9bea99631af017e4e0b9fcd1d1979222bd3721b
RolandMario <rolandmario2@gmail.com>
2025-10-09 23:57:16 +0100

    update2

--- client/src/components/StudentAttendance.jsx
+++ client/src/components/StudentAttendance.jsx
@@ -43,7 +43,7 @@ export default function StudentAttendance() {
       }
     }
     fetchStudents();
-  }, []);
+  }, [students]);
 
   const handleStatusChange = (index, value) => {
     const updated = [...records];
