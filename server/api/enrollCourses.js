e4d4332fdafc54962c8fc96cdba443dd6df80a4e
RolandMario <rolandmario2@gmail.com>
2025-11-17 18:05:02 +0100

    added req headers

--- server/api/enrollCourses.js
+++ server/api/enrollCourses.js
@@ -2,6 +2,20 @@ import Student from '../models/Student';
 import dbConnect from '../lib/db';
 
 export default async function handler(req, res) {
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end();
+    return;
+  }
+
+   // Main response headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
--- server/api/fetchCourses.js
+++ server/api/fetchCourses.js
@@ -2,6 +2,20 @@
 import Course from '../models/Course'
 import dbConnect from '../lib/db';
 export default async function handler(req, res) {
+
+    if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end();
+    return;
+  }
+
+   // Main response headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if(!req.method==='GET') res.status(400).json({error: 'Method Not allowed'})
 
     try {
